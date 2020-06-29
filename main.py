bb66c095af21167ab329a226bf79fad33e175845
xilo228 <ldzkeymylp@gmail.com>
2020-06-29 21:13:21 +0300

    Update BIG
    A cho

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from settings import TOKEN
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -12,9 +11,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!'):
+    if message.content.startswith('!join'):
         await message.channel.send(message.content)
+        channel = message.author.voice.channel
+        print(channel.name)
+        await channel.connect(timeout=10.0,reconnect=True)
 
+@client.event
+async def on_voice_state_update(member,before,after):
+    print ('1')
 
 
 
