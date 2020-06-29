6f1cdaf6fb853cd85795519cd8f74cf6090e8b3f
xilo228 <ldzkeymylp@gmail.com>
2020-06-29 19:47:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import discord
-from discord.ext import commands
+from settings import TOKEN
 
-import settings
+client = discord.Client()
 
-bot = commands.Bot(command_prefix='!')
-@bot.command()
-async def ping(ctx, arg):
-    await ctx.send(arg)
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-bot.run(settings.TOKEN)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!'):
+        await message.channel.send(message.content)
+
+
+
+
+client.run(TOKEN)
