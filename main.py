5ae28f19e891d798c479327bba66e328866fcb19
bronmal <48320414+bronmal@users.noreply.github.com>
2020-06-29 21:39:19 +0300

    disconnect

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+voice = None
 @client.event
 async def on_message(message):
+    global voice
     if message.author == client.user:
         return
 
     if message.content.startswith('!join'):
-        await message.channel.send(message.content)
         channel = message.author.voice.channel
-        print(channel.name)
-        await channel.connect(timeout=10.0,reconnect=True)
+        voice = await channel.connect(timeout=10.0,reconnect=True)
 
-@client.event
-async def on_voice_state_update(member,before,after):
-    print ('1')
+    if message.content.startswith('!dis'):
+        channel = message.author.voice.channel
+        await voice.disconnect(force=False)
 
 
 
