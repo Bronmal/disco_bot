b5211df7d7f82e39fc2e323b4be844f06538ba6b
bronmal <48320414+bronmal@users.noreply.github.com>
2020-06-30 10:28:11 +0300

    Играет музыку

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import discord
 from settings import TOKEN
 
@@ -11,12 +12,28 @@ voice = None
 @client.event
 async def on_message(message):
     global voice
-    if message.author == client.user:
-        return
+    if message.author.id == 461173185916960768 or message.author.id == 467398633755770890 and message.content =='!quit':
+        sys.exit()
 
     if message.content == '!join':
         channel = message.author.voice.channel
+        print(message.author.id)
         voice = await channel.connect(timeout=10.0,reconnect=True)
+    
+    if message.content == '!play':
+        voice.play(discord.FFmpegPCMAudio('1.mp3'), after=None)
+    
+
+    if message.content == '!pause' and voice.is_playing():
+        voice.pause()
+
+    
+    if message.content == '!resume' and voice.is_paused():
+        voice.resume()
+
+    if message.content == '!stop':
+        voice.stop()
+
 
     if message.content == '!disconnect':
         await voice.disconnect(force=False)
